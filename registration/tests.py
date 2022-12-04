08d90e3af476fdca1540db41dca63b4310a0a3a8
Ahmed Mohamed <ahmedysatti@gmail.com>
2022-12-04 20:57:38 +0000

    Registartion page test

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import SimpleTestCase
+
+class poll_page_Tests(SimpleTestCase):
+    
+    def test_registration_page_response_is_200(self):
+        response = self.client.get('http://127.0.0.1:8000/registration/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_registration_page_text(self):
+        response = self.client.get('http://127.0.0.1:8000/registration/')
+        self.assertContains(response, 'Registartion form')
